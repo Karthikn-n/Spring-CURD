75fa669f3dd570f876998ace6b94ace5d67caead
Karthikn-n <karthikn11092001@gmail.com>
2025-04-12 19:00:14 +0530

    Comments added

--- src/main/java/com/example/helloworld/util/AESUtil.java
+++ src/main/java/com/example/helloworld/util/AESUtil.java
@@ -9,9 +9,13 @@ public class AESUtil {
     private static final String ALGORITHM = "AES";
 
     public static String encrypt(String data) throws Exception {
+        /// instantiating the algorithm method from the Cipher Java in-built method
         Cipher cipher = Cipher.getInstance(ALGORITHM);
+        ///  Construct the Secret key with the given Key bytes
         SecretKeySpec key = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        ///  Initialize the Java Cipher Object with Key
         cipher.init(Cipher.ENCRYPT_MODE, key);
+        /// Encrypt the data into single final file
         byte[] encrypted = cipher.doFinal(data.getBytes());
         return Base64.getEncoder().encodeToString(encrypted);
     }
